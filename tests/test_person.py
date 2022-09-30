5aae558be06078a9e004e1ab2acb9ae9d643019f
June Valentino <vintydesign@gmail.com>
2022-09-30 11:42:30 -0700

    we made a thing

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,18 @@
 import pytest
+from viewing_party.person import Person
 
-def test_1():
+def test_add_movie():
     # Arrange
-
+    name1 = "Kendall"
+    watched1 = ["Movie1", "Movie2", "Movie3"]
+    friends1 = ["Jasmine"]
+    subscriptions1 = ["Netflix", "Hulu"]
+    person1 = Person(name1, watched1, friends1, subscriptions1)
+    movie4 = "Movie4"
+    
     # Act
+    person1.add_watched(movie4)
 
     # Assert
-    pass
+    assert len(person1.watched) == 4
+    assert person1.watched == ["Movie1", "Movie2", "Movie3", "Movie4"]
